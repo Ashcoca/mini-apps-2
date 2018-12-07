ffaea8d11a1be72c7c31ff315a7332b250a94aa0
Ashvin Coca <AshvinCoca@gmail.com>
2018-12-06 23:09:13 -0800

    Added coinbase text and link

--- challenge_2/client/components/App.jsx
+++ challenge_2/client/components/App.jsx
@@ -14,7 +14,7 @@ class App extends React.Component {
       error: null
     }; 
     this.enterDate = this.enterDate.bind(this);
-  }
+  };
 
   componentDidMount() {
     fetch('/currentprice')
@@ -24,60 +24,66 @@ class App extends React.Component {
         labels: Object.keys(data.bpi),
         values: Object.values(data.bpi),
         error: null,
-      })
+      });
     })
     .catch(error => this.setState({ error, isLoading: false}))
   };
 
   componentDidUpdate() {
-    const chartData = {
-      labels: this.state.labels,
-      datasets: [
-        {
-          label: 'Bitcoin Price',
-          fill: true,
-          lineTension: 0.1,
-          backgroundColor: 'rgba(75,192,192,0.4)',
-          borderColor: 'rgba(75,192,192,1)',
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'rgba(75,192,192,1)',
-          pointBackgroundColor: '#fff',
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-          pointHoverBorderColor: 'rgba(220,220,220,1)',
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: this.state.values
-        }
-      ]
+    if (!this.state.isLoading) {
+      return;
+    } else {
+      const chartData = {
+        labels: this.state.labels,
+        datasets: [
+          {
+            label: 'Bitcoin Price',
+            fill: true,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 20,
+            data: this.state.values
+          }
+        ]
+      };
+      this.setState({
+        data: chartData,
+        isLoading: false
+      });
     };
-    this.setState({
-      data: chartData,
-      isLoading: false
-    });
   };
 
   enterDate() {
     console.log(this.state.start)
     console.log(this.start.end)
-  }
+  };
 
   render() {
     if (!this.state.isLoading) {
       return (
         <div>
           <Chart data={this.state.data}/>
-          <label>Enter start date: 
-            <input type="date" value={this.state.start} onChange={this.enterDate}></input>
-          </label>
-          <label> Enter end date: 
-            <input type="date" value={this.state.end} onChange={this.enterDate}></input>
-          </label>
+            <label>Enter start date: 
+              <input type="date"></input>
+            </label>
+            <label> Enter end date: 
+              <input type="date"></input>
+            </label>
+
+            <h5><a href="https://www.coindesk.com/price/">Powered by CoinDesk</a></h5>
         </div>
       );
     } else {
@@ -88,7 +94,6 @@ class App extends React.Component {
       );
     };
   };
-
 };
 
 export default App;
