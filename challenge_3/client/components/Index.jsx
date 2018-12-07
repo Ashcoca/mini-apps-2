3f27509dfaf1d23d0c522695af918b8f6e106874
Ashvin Coca <AshvinCoca@gmail.com>
2018-12-07 12:29:49 -0800

    Keeping score, now to add actual bowling rules to scorekeeping

--- challenge_3/client/components/Index.jsx
+++ challenge_3/client/components/Index.jsx
@@ -1,23 +1,66 @@
-import React from 'react'
+import React from 'react';
+import Pins from './PinGrid.jsx';
 
 class Bowling extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      score: [],
+      pendingScore: 0,
+      turn: 1,
       isLoading: true,
       error: null
     }
+    this.pinsHit = this.pinsHit.bind(this);
+  }
+
+  componentDidMount() {
+    
+  }
+
+  pinsHit(e) {
+    let hit = parseInt(e.target.innerText)
+    if (this.state.turn === 1) {
+      this.setState({
+        pendingScore: hit,
+        turn: 2
+      });
+    };
+    if (this.state.turn === 2) {
+      hit += this.state.pendingScore;
+      this.setState({
+        score: this.state.score.concat([hit]),
+        turn: 1,
+        pendingScore: 0
+      });
+    };
   }
 
   render() {
-    return (
-      <div>
-        Page is Loaded!
-      </div>
-    )
+    if (this.state.isLoading) {
+      return (
+        <div>
+          <Pins click={this.pinsHit}/>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <img src='https://thumbs.gfycat.com/FaithfulDeafeningBullmastiff-small.gif'></img>
+        </div>
+      )
+    }
   }
 
 
 }
 
 export default Bowling;
+
+
+
+/*
+<img class="bowling-ball" src="https://lh3.google.com/u/0/d/0B1-GEHdXK13VcnVGOGh5VmxFM1U=w2560-h1310-iv1"/>
+<img class="bowling-pin-1" src="https://lh3.google.com/u/0/d/0B1-GEHdXK13VX0V2Q2g4Ry03RUE=w2560-h1310-iv1"/>
+
+*/
---
+++ challenge_3/client/components/PinGrid.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Pins = (props) => (
+  <div className="pinbox" onClick={props.click}>
+    <span className="pins">1</span>
+    <span className="pins">2</span>
+    <span className="pins">3</span>
+    <br/>
+    <span className="pins">4</span>
+    <span className="pins">5</span>
+    <span className="pins">6</span>
+    <br/>
+    <span className="pins">7</span>
+    <span className="pins">8</span>
+    <span className="pins">9</span>
+    <br/>
+    <span className="pins">10</span>
+  </div>
+);
+
+export default Pins;
